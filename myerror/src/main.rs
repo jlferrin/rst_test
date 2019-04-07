85e89a79b91b3d5d162a03a428aee8249373af4f
juanillo <juanillo@muralla.feloz.local>
2019-04-07 13:33:51 +0200

    more on errors

--- myerror/src/main.rs
+++ myerror/src/main.rs
@@ -31,10 +31,130 @@ fn give_princess2(gift: Option<&str>) {
     println!("I love {}s!!!!!", inside);
 }
 
+// Combinators: map
+// -------------------
+#[derive(Debug)] enum Food { Apple, Carrot, Potato }
+
+#[derive(Debug)] struct Peeled(Food);
+#[derive(Debug)] struct Chopped(Food);
+#[derive(Debug)] struct Cooked(Food);
+
+// Peeling food. If there isn't any, then return `None`.
+// Otherwise, return the peeled food.
+fn peel(food: Option<Food>) -> Option<Peeled> {
+    match food {
+        Some(food) => Some(Peeled(food)),
+        None       => None,
+    }
+}
+
+// Chopping food. If there isn't any, then return `None`.
+// Otherwise, return the chopped food.
+fn chop(peeled: Option<Peeled>) -> Option<Chopped> {
+    match peeled {
+        Some(Peeled(food)) => Some(Chopped(food)),
+        None               => None,
+    }
+}
+
+// Cooking food. Here, we showcase `map()` instead of `match` for case handling.
+fn cook(chopped: Option<Chopped>) -> Option<Cooked> {
+    chopped.map(|Chopped(food)| Cooked(food))
+}
+
+// A function to peel, chop, and cook food all in sequence.
+// We chain multiple uses of `map()` to simplify the code.
+fn process(food: Option<Food>) -> Option<Cooked> {
+    food.map(|f| Peeled(f))
+        .map(|Peeled(f)| Chopped(f))
+        .map(|Chopped(f)| Cooked(f))
+}
+
+// Check whether there's food or not before trying to eat it!
+fn eat(food: Option<Cooked>) {
+    match food {
+        Some(food) => println!("Mmm. I love {:?}", food),
+        None       => println!("Oh no! It wasn't edible."),
+    }
+}
+
+// Combinators: and_then
+// -------------------
+#[derive(Debug)] enum Food2 { CordonBleu, Steak, Sushi }
+#[derive(Debug)] enum Day { Monday, Tuesday, Wednesday }
+
+// We don't have the ingredients to make Sushi.
+fn have_ingredients(food: Food2) -> Option<Food2> {
+    match food {
+        Food2::Sushi => None,
+        _           => Some(food),
+    }
+}
+
+// We have the recipe for everything except Cordon Bleu.
+fn have_recipe(food: Food2) -> Option<Food2> {
+    match food {
+        Food2::CordonBleu => None,
+        _                => Some(food),
+    }
+}
+
+// To make a dish, we need both the ingredients and the recipe.
+// We can represent the logic with a chain of `match`es:
+fn cookable_v1(food: Food2) -> Option<Food2> {
+    match have_ingredients(food) {
+        None       => None,
+        Some(food) => match have_recipe(food) {
+            None       => None,
+            Some(food) => Some(food),
+        },
+    }
+}
+
+// This can conveniently be rewritten more compactly with `and_then()`:
+fn cookable_v2(food: Food2) -> Option<Food2> {
+    have_ingredients(food).and_then(have_recipe)
+}
+
+fn eat2(food: Food2, day: Day) {
+    match cookable_v2(food) {
+        Some(food) => println!("Yay! On {:?} we get to eat {:?}.", day, food),
+        None       => println!("Oh no. We don't get to eat on {:?}?", day),
+    }
+}
+
 
 fn main() {
 
+    // Combinators: and_then
+    // -------------------
+    let (cordon_bleu, steak, sushi) = (Food2::CordonBleu, Food2::Steak, Food2::Sushi);
+
+    eat2(cordon_bleu, Day::Monday);
+    eat2(steak, Day::Tuesday);
+    eat2(sushi, Day::Wednesday);
+
+    println!("");
+
+    // Combinators: map
+    // -------------------
+    let apple = Some(Food::Apple);
+    let carrot = Some(Food::Carrot);
+    let potato = None;
+
+    let cooked_apple = cook(chop(peel(apple)));
+    let cooked_carrot = cook(chop(peel(carrot)));
+    // Let's try the simpler looking `process()` now.
+    let cooked_potato = process(potato);
+
+    eat(cooked_apple);
+    eat(cooked_carrot);
+    eat(cooked_potato);
+    
+    println!("");
+
     // option & unwrap
+    // -------------------
     let food  = Some("cabbage");
     let snake = Some("snake");
     let void  = None;
@@ -49,7 +169,14 @@ fn main() {
     give_princess2(bird);
     give_princess2(nothing);
 
+    println!("Panic previous");
+
     // panic
+    // -------------------
+    let food  = Some("cabbage");
     give_princess("teddy bear");
     give_princess("snake");
+
+    println!("Panic previous");
+
 }
