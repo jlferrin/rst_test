404c3ba8be89bb0acdecfe4f125849221e2363a1
juanillo <juanillo@muralla.feloz.local>
2019-04-09 22:25:10 +0200

    myerror result

--- myerror/src/main.rs
+++ myerror/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 
 // panic
 // -------------------
@@ -123,8 +124,100 @@ fn eat2(food: Food2, day: Day) {
     }
 }
 
+// Result
+// -------------------
+
+use std::fs::File;
+use std::io::ErrorKind;
+
+// Propagating
+// -------------------
+
+use std::io;
+use std::fs;
+use std::io::Read;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// The same function using ?
+// The ? Operator Can Only Be Used in Functions That Return Result
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+// The same with ? but ultra-simple
+fn read_username_from_file3() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+// The real implementation 
+fn read_username_from_file4() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
 
 fn main() {
+
+    // Propagating
+    // -------------------
+
+    
+    // Result
+    // -------------------
+
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+    // - Single error ----
+    //    Err(error) => {
+    //        panic!("There was a problem opening the file: {:?}", error)
+    //    },
+    // - Different errors -
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("../../data/hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+            },
+            other_error => panic!("There was a problem opening the file: {:?}", other_error),
+        },
+    };
+
+    let f2 = File::open("hello.txt").map_err(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("../../data/hello.txt").unwrap_or_else(|error| {
+                panic!("Tried to create file but there was a problem: {:?}", error);
+            })
+        } else {
+            panic!("There was a problem opening the file: {:?}", error);
+        }
+    });
+
+    // If OK ft gets file, if Err then calls panic!
+    let f3 = File::open("hello.txt").unwrap();
+
+    // If OK ft gets file, if Err then calls panic! with the message
+    let f4 = File::open("hello.txt").expect("Failed to open hello.txt");
+
 
     // Combinators: and_then
     // -------------------
